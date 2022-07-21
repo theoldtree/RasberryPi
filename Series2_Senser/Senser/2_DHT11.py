751ea734c471cff6db5dedee3df4836f812b3995
theoldtree <yjeongmu@gmail.com>
2022-07-21 10:14:28 +0900

    온습도 센서 코드 수정

--- Series2_Senser/Senser/2_DHT11.py
+++ Series2_Senser/Senser/2_DHT11.py
@@ -3,16 +3,15 @@ import board
 import adafruit_dht
 
 dhtDevice = adafruit_dht.DHT11(board.D18)
+
 while True:
-    try:
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
+    try: 
+        temperature = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity))
+        print(temperature, humidity)
         time.sleep(2.0)
-        
-  except KeyboardInterrupt:
-    pass
-    print('Exit with ^C. Goodbye!')
-    exit()
+    except KeyboardInterrupt:
+        pass
+        print("Terminated by Keyboard")
+        exit()
+                                            
